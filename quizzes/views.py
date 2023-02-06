4b661d44802aaf60fade95dc9bb33402504108c0
ArnavJain2709 <73390580+ArnavJain2709@users.noreply.github.com>
2023-02-06 21:23:13 +0000

    modified:   Pipfile 	modified:   Pipfile.lock 	modified:   db.sqlite3 	modified:   quizzes/views.py 	modified:   templates/authentication/index.html 	new file:   templates/quizzes/index.html

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.contrib import messages
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("This view function is working.")
+
+    if request.user.is_authenticated:
+        username = request.user.username
+        fname = request.user.username
+
+        return render(request, "quizzes/index.html", {'fname': fname})
+    else:
+        messages.error(request, "Please Login / Register!")
+        return redirect('home')
