a6a0334dc867315de2ad1681140d8ece7afaf64f
ArnavJain2709 <73390580+ArnavJain2709@users.noreply.github.com>
2023-04-16 00:16:00 +0100

    Updated STATIC_ROOT setting in settings.py

--- Revision_web_app/settings.py
+++ Revision_web_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from . info import *
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +133,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Specify a valid path for STATIC_ROOT where static files should be collected during deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
